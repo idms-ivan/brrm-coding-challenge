9313feb2885a6762fe2dd7fb35ef5a9ee95c0b0b
DESKTOP-GU54A8M\Ivan Kovacevic <i.kovacevic@exafysolutions.ae>
2025-07-02 11:31:31 +0200

    create database if doesnt exists

--- src/database/index.js
+++ src/database/index.js
@@ -28,21 +28,44 @@ const establishDatabaseConnection = async () => {
     return;
   }
   try {
+    const checkDbConnection = new Sequelize(
+      'postgres',
+      config.databaseUserName,
+      config.databasePassword,
+      {
+        host: config.databaseHost,
+        dialect: "postgres",
+      }
+    );
+
+    await checkDbConnection.authenticate();
+    console.log("Connected to PostgreSQL successfully.");
+
+    const [results, metadata] = await checkDbConnection.query(`
+      SELECT 1 FROM pg_database WHERE datname = '${config.databaseName}'
+    `);
+
+    if (results.length === 0) {
+      console.log(`Database "${config.databaseName}" does not exist. Creating now...`);
+      await checkDbConnection.query(`CREATE DATABASE ${config.databaseName}`);
+      console.log(`Database "${config.databaseName}" created successfully.`);
+    } else {
+      console.log(`Database "${config.databaseName}" already exists.`);
+    }
+
     await sequelize.authenticate();
     console.log("Database connection has been established successfully.");
     connectionEstablished = true;
+
+    const _models = attachModels(sequelize);
+    _.assign(models, _models);
+
+    await sequelize.sync({ alter: true });
   } catch (error) {
     console.log("Unable to connect to the database:");
     console.log(error.message);
     process.exit(1);
   }
-
-  const _models = attachModels(sequelize);
-  _.assign(models, _models);
-
-  await sequelize.sync({
-    alter: true,
-  });
 };
 
 module.exports = {
